04a8dbccf26a42f6fdbeff77419f3dbf0eda7c82
Kaylin Chu <kaylinltj@hotmail.com>
2025-07-11 00:39:26 +1200

    Social Media Pics Emotional Intelligence

---
+++ index.js
@@ -0,0 +1,13 @@
+
+//<a href="#">
+// <img
+// src="https://media.licdn.com/dms/image/v2/C4E03AQE0K_CEPYLPfQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1660810074718?e=2147483647&v=beta&t=3A09KPEr6516lkxM3c_Za8IoyXyP178Ch2esJ-2LDz0"
+// class="round-image"
+// />
+// </a>
+
+// let roundImage = document.getElementsByClassName('round-image')
+// roundImage.onclick = clickImage
+// function clickImage () {
+
+// }
