03cce496cdf94ede9355173de1431cb66bcefa06
Kaylin Chu <kaylinltj@hotmail.com>
2025-07-15 11:56:41 +1200

    Enchantment?

---
+++ Sprint6/Sprint5Stretch-PlayWithJS/index.js
@@ -0,0 +1,6 @@
+import canvasConfetti from 'https://cdn.skypack.dev/canvas-confetti'
+
+let confetti = document.getElementById('confetti')
+
+confetti.onclick = console.log(confetti)
+canvasConfetti()
