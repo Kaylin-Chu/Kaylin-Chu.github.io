f3782f804f0b5e809b0c3369125fe5207858df60
Kaylin Chu <kaylinltj@hotmail.com>
2025-07-15 15:31:36 +1200

    Timeout attempt

--- Sprint6/Sprint5Stretch-PlayWithJS/index.js
+++ Sprint6/Sprint5Stretch-PlayWithJS/index.js
@@ -4,13 +4,9 @@ let confetti = document.getElementById('confetti')
 let star = document.getElementById('star')
 let music = document.getElementById('music')
 
-
-
 confetti.onclick = clickCannon
 star.onclick = clickStar
 music.onclick = clickMusic
-
-
 
 function clickCannon() {
   canvasConfetti()
@@ -18,46 +14,29 @@ function clickCannon() {
   soundEffect.play()
 }
 
-function clickStar () {
+function clickStar() {
   shoot()
+  setTimeout(shoot, 0)
+  setTimeout(shoot, 100)
+  setTimeout(shoot, 200)
   let soundEffect = new Audio('Twinkle.mp3')
   soundEffect.play()
 }
 
-function clickMusic () {
+function clickMusic() {
   let soundEffect = new Audio('Music.mp3')
   soundEffect.play()
 }
 
-
-
-
-
-let defaults = {
-  spread: 360,
-  ticks: 50,
-  gravity: 0,
-  decay: 0.94,
-  startVelocity: 30,
-  colors: ['FF1493', 'FFC0CB', 'FF69B4']
-};
-
 function shoot() {
   confetti({
-    ...defaults,
+    spread: 360,
+    ticks: 50,
+    gravity: 0,
+    decay: 0.94,
+    startVelocity: 30,
+    colors: ['FF1493', 'FFC0CB', 'FF69B4'],
     particleCount: 40,
-    scalar: 1.2,
-    shapes: ['star']
-  });
-
-  confetti({
-    ...defaults,
-    particleCount: 10,
-    scalar: 0.75,
-    shapes: ['circle']
-  });
+    shapes: ['star'],
+  })
 }
-
-setTimeout(shoot, 0);
-setTimeout(shoot, 100);
-setTimeout(shoot, 200);
